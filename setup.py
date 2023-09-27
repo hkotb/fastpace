2c27ebc902a88fac25420f6933d7e48ee768bb61
Hazem <hazem.mamdouh@icr.ac.uk>
2023-09-27 12:26:21 +0100

    update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='fastpace',
-    version='0.1.0',
+    version='0.2.0',
     author="Hazem M. Kotb",
     author_email="hazem.mamdouh.kotb@gmail.com",
     description="FaSTPACE: A Fast and Scalable Tool for Peptide Alignment and Consensus Extraction",
