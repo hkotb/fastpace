567cdf09c1b641562243b76d2b67165eb33b8cce
Hazem <hazem.mamdouh@icr.ac.uk>
2023-10-31 01:52:47 +0000

    add -std=c++14 as compile flag to avoid defaults in clang

--- setup.py
+++ setup.py
@@ -10,9 +10,13 @@ file_path = os.path.dirname(inspect.stack()[0][1])
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Clang builds C++ code according to the C++98 standard, with many C++11 features accepted as extensions
+# Add std=c++14 to override this behavior and build according to the C++14 standard
+extra_compile_args = ["-std=c++14"]
+
 setup(
     name='fastpace',
-    version='0.4.0',
+    version='0.5.0',
     author="Hazem M. Kotb",
     author_email="hazem.mamdouh.kotb@gmail.com",
     description="FaSTPACE: A Fast and Scalable Tool for Peptide Alignment and Consensus Extraction",
@@ -28,5 +32,7 @@ setup(
         Extension('fastpace', [
             os.path.join(file_path, 'src', 'fastpace_module_main.c'), 
             os.path.join(file_path, 'src', 'fastpace_utils.c'),
-            os.path.join(file_path, 'src', 'scipy_interface.cpp')])
+            os.path.join(file_path, 'src', 'scipy_interface.cpp')],
+            extra_compile_args=extra_compile_args
+        )
     ])
